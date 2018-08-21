3bec9c7ea58f1ef3b1799d08121cba833a44d335
shinyay <shinya.com@gmail.com>
2018-08-21 16:41:32 +0900

    Initial commit for configuration server

---
+++ src/main/kotlin/io/pivotal/shinyay/config/ConfigApplication.kt
@@ -0,0 +1,13 @@
+package io.pivotal.shinyay.config
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ConfigApplication::class.java, *args)
+}
